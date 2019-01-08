d3be6727311f35b9ab440ab0078de3cc66ce8711
PoetralesanA <poetralesana13@gmail.com>
2019-01-08 17:12:05 +0800

    ==Convert===

---
+++ Mata Uang Convert/Form1.Designer.vb
@@ -0,0 +1,238 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.cmb_dari = New System.Windows.Forms.ComboBox()
+        Me.cmb_ke = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(15, 289)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(385, 39)
+        Me.Button1.TabIndex = 3
+        Me.Button1.TabStop = False
+        Me.Button1.Text = "Convert"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(7, 29)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(369, 29)
+        Me.TextBox1.TabIndex = 0
+        '
+        'cmb_dari
+        '
+        Me.cmb_dari.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmb_dari.FormattingEnabled = True
+        Me.cmb_dari.Location = New System.Drawing.Point(7, 29)
+        Me.cmb_dari.Name = "cmb_dari"
+        Me.cmb_dari.Size = New System.Drawing.Size(369, 25)
+        Me.cmb_dari.TabIndex = 1
+        '
+        'cmb_ke
+        '
+        Me.cmb_ke.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmb_ke.FormattingEnabled = True
+        Me.cmb_ke.Location = New System.Drawing.Point(9, 75)
+        Me.cmb_ke.Name = "cmb_ke"
+        Me.cmb_ke.Size = New System.Drawing.Size(369, 25)
+        Me.cmb_ke.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(190, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 17)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Nilai"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Panel1)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.cmb_dari)
+        Me.GroupBox1.Controls.Add(Me.cmb_ke)
+        Me.GroupBox1.Location = New System.Drawing.Point(15, 170)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(384, 113)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Convert"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Gainsboro
+        Me.Panel1.Location = New System.Drawing.Point(12, 59)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(365, 1)
+        Me.Panel1.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI Semibold", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(182, 59)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(20, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Ke"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI Semibold", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(178, 13)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(28, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Dari"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(16, 95)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(383, 75)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Image = CType(resources.GetObject("LinkLabel1.Image"), System.Drawing.Image)
+        Me.LinkLabel1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.LinkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline
+        Me.LinkLabel1.Location = New System.Drawing.Point(15, 331)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(79, 17)
+        Me.LinkLabel1.TabIndex = 8
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "         GitHub    "
+        Me.LinkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.LinkLabel1.UseCompatibleTextRendering = True
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Image = CType(resources.GetObject("LinkLabel2.Image"), System.Drawing.Image)
+        Me.LinkLabel2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.LinkLabel2.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline
+        Me.LinkLabel2.Location = New System.Drawing.Point(100, 331)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(87, 17)
+        Me.LinkLabel2.TabIndex = 9
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "         Facebook  "
+        Me.LinkLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.LinkLabel2.UseCompatibleTextRendering = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(16, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(384, 77)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(19, 74)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(120, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Coderz : ./PoetralesanA"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(414, 351)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.LinkLabel2)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Mata Uang Converter"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents cmb_dari As System.Windows.Forms.ComboBox
+    Friend WithEvents cmb_ke As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents LinkLabel2 As System.Windows.Forms.LinkLabel
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Mata Uang Convert/Form1.vb
@@ -0,0 +1,86 @@
+﻿Public Class Form1
+    Dim indo, usa As String
+#Region "messages"
+    'Hallo Shifu.. Selamat mengembangkan..
+    'Hak karyanya jangan dihilangkan ya.. Terimakasih..
+#End Region
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.TextAlign = HorizontalAlignment.Center
+
+        '///////// VARIABLE /////////'
+        indo = "Indonesia (IDR)"
+        usa = "USA (Dolar)"
+        '//////////////////////////'
+
+        With cmb_dari.Items  '// Combobox Dari
+            .Add(usa) 'index 0
+            .Add(indo) 'index 1
+        End With
+
+        With cmb_ke.Items '// Combobox Ke
+            .Add(indo) ' index 0
+            .Add(usa) ' index 1
+        End With
+    End Sub
+
+    Private Sub btnConvert(sender As Object, e As EventArgs) Handles Button1.Click
+        'penjelasan
+        ' 1 dolar = 14350rb (rupiah)
+
+        If TextBox1.Text = "" Then
+            MessageBox.Show(
+                            "Value belum dimasukan!", "", MessageBoxButtons.OK,
+                                                                MessageBoxIcon.Error
+                                )
+            TextBox1.Focus()
+        ElseIf Not IsNumeric(TextBox1.Text) Then
+            MessageBox.Show(
+                            "Numberic!!!", "", MessageBoxButtons.OK,
+                                                                MessageBoxIcon.Error
+                            )
+            TextBox1.Clear()
+            TextBox1.Focus()
+        ElseIf cmb_dari.Text = "" Or
+                        cmb_ke.Text = "" Then
+            MessageBox.Show(
+                            "Anda Belum memilih data" & vbNewLine & "Silahkan memilih data yang ingin di Convert!",
+                            "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation
+                            )
+        ElseIf cmb_dari.Text = usa And cmb_ke.Text = usa Or
+             cmb_dari.Text = indo And cmb_ke.Text = indo Then
+            MessageBox.Show(
+                            "Data Tidak Boleh Sama!", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation
+                            )
+        Else
+
+            Dim dolar = 14100, tampil As Integer
+            Dim _double = 1, _tampil As Double '// < convert 0,
+
+            If cmb_dari.SelectedIndex = 0 And cmb_ke.SelectedIndex = 0 Then
+                tampil = Val(TextBox1.Text) * dolar
+                MsgBox(
+                        "Hasil Conversi dari " & usa & " > " & indo & vbNewLine &
+                        tampil & " Rupiah", MsgBoxStyle.Information, ""
+                        )
+            ElseIf cmb_dari.SelectedIndex = 1 And cmb_ke.SelectedIndex = 1 Then
+                _tampil = _double / Val(TextBox1.Text)
+                MsgBox(
+                       "Hasil Conversi dari " & indo & " > " & usa & vbNewLine _
+                       & _tampil & " Dolar", MsgBoxStyle.Information, ""
+                        )
+            End If
+        End If
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Process.Start("https://www.facebook.com/PoetralesanA")
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://www.github.com/PoetralesanA")
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        MsgBox("./PoetralesanA", MsgBoxStyle.Information, "n00b Coderz")
+    End Sub
+End Class
